cc226ba76275eed6124c5605b05d483a9b64d205
Nilansh Rajput <rajputnilansh@gmail.com>
2018-06-08 13:29:28 +0530

    Add files via upload

--- facne.x.py
+++ facne.x.py
@@ -1,6 +1,40 @@
 import cv2
 import sys
 
+
+
+from naoqi import ALProxy
+import vision_definitions
+import time
+
+IP = "192.168.43.73"  # Replace here with your NAOqi's IP address.
+PORT = 9559
+
+####
+# Create proxy on ALVideoDevice
+
+print "Creating ALVideoDevice proxy to ", IP
+
+camProxy = ALProxy("ALVideoDevice", IP, PORT)
+
+
+resolution = vision_definitions.kQQVGA
+colorSpace = vision_definitions.kYUVColorSpace
+fps = 20
+
+nameId = camProxy.subscribe("python_GVM", resolution, colorSpace, fps)
+
+  
+  
+
+camProxy.unsubscribe(nameId)
+
+
+
+
+
+
+
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
@@ -8,16 +42,12 @@ video_capture = cv2.VideoCapture(0)
 
 while True:
    
-    ret, frame = video_capture.read()
-
+    #ret, frame = video_capture.read()
+    frame=camProxy.getImageRemote(nameId)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5, minSize=(30, 30),
+	flags=cv2.CASCADE_SCALE_IMAGE
     )
 
     
@@ -26,8 +56,10 @@ while True:
 
     cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('a'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+camProxy.unsubscribe(nameId)
+
 
 
 video_capture.release()
